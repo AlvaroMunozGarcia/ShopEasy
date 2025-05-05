cd08e7e20648d172c77e8f8a37097e285597fbe3
Alvaro <alvaromunozgarcia585@gmail.com>
2025-05-05 10:39:31 +0200

    Modificacion de ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,19 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sale;
+use Carbon\Carbon;
 
 class ReportController extends Controller
 {
     public function reports_day()
     {
-        return view('admin.report.day');
+        $sales=Sale::whereDate('sale_date',Carbon::today('America/Lima'))->get();
+        $total =$sales->sum('total');
+        return view('admin.report.reports_day',compact('sales','total'));
     }
     public function reports_date()
     {
-        return view('admin.report.date');
+        $sales=Sale::whereDate('sale_date',Carbon::today('America/Lima'))->get();
+        $total =$sales->sum('total');
+        return view('admin.report.reports_date',compact('sales','total'));
+
     }
-    public function report_results()
+    public function report_results(Request $request)
     {
-        return view('admin.report.results');
+        $fi =$request->fecha_ini.'00:00:00';
+        $ff =$request->fecha_fin.'23:59:59';
+        $sales=Sale::whereBetween('sale_date',$fi,$ff)->get();
+        $total =$sales->sum('total');
+        return redirect()->route('reports.date',compact('sales','total'));
     }
 }
